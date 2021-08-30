04be0b13d1c8be0049e8940d0150da26d069b8f1
StianOek <66445281+StianOek@users.noreply.github.com>
2021-08-30 21:09:19 +0200

    Create Main Method. Resolves #1

---
+++ src/main/java/no/main/Main.java
@@ -0,0 +1,13 @@
+package no.main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Your about to have romans thrown in your face");
+
+        Scanner input = new Scanner(System.in);
+        int user = input.nextInt();
+        System.out.println(user + " is not a roman i presume");
+    }
+}
