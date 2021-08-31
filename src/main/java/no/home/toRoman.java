c64c6ecb0ad9d8aed9803896ab00d2c22a8e9c69
StianOek <66445281+StianOek@users.noreply.github.com>
2021-08-31 08:28:08 +0200

    shouldConvert2ToIII ( pass )

--- src/main/java/no/home/toRoman.java
+++ src/main/java/no/home/toRoman.java
@@ -5,7 +5,9 @@ public class toRoman {
     public String convert(int i) {
         if(i == 1) {
             return "I";
+        } else if( i == 2) {
+            return "II";
         }
-        return "II";
+        return "III";
     }
 }
