7a621d24d0a6a194ee88fb74a17b6baf3fbef3bf
StianOek <66445281+StianOek@users.noreply.github.com>
2021-08-31 08:52:20 +0200

    shouldConvert2ToIII ( Refactor )

--- src/main/java/no/home/toRoman.java
+++ src/main/java/no/home/toRoman.java
@@ -3,11 +3,11 @@ package no.home;
 public class toRoman {
 
     public String convert(int i) {
-        if(i == 1) {
-            return "I";
-        } else if( i == 2) {
-            return "II";
+        String result = "";
+        for (int j = 0; j < i; j++) {
+            result += "I";
         }
-        return "III";
+
+        return result;
     }
 }
